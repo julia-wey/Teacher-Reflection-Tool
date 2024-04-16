cf1bf41f69e9f560df10bcd19eeee9ae6fca763b
Julia Wey-Burgoyne <juliawey@gmail.com>
2024-04-16 14:43:36 -0400

    add completed files

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 fetch("http://localhost:3000/criteria")
     .then((resp)=> resp.json())
-    .then((data) => displayCriteria(data))
+    .then((data) => displayCriteria(data));
 
     //Run json-server --watch db.json
     
 function displayCriteria(criteriaArr) {
         
-    const critA = document.querySelector("#strands1")
+    const critA = document.querySelector("#strands1");
         
-    critA.addEventListener("mouseover", displayCritA)
+    critA.addEventListener("mouseover", displayCritA);
 
     function displayCritA() {
         const critAStrands = criteriaArr.filter((data) => 
@@ -26,131 +26,149 @@ function displayCriteria(criteriaArr) {
         const critATwo = document.querySelector("#p2");
         critATwo.textContent = strandA2;
         
-        critA.addEventListener("mouseout", hideCritA)
+        critA.addEventListener("mouseout", hideCritA);
             function hideCritA() {
                 intro1.textContent = ' '
                 critAOne.textContent = ' '
                 critATwo.textContent = ' '
         };
-    }
-    //QUESTION- why does the first event stop working after mouseout event? 
-    const critB = document.querySelector("#strands2")
+    };
     
-    critB.addEventListener("mouseover", displayCritB)
+    const critB = document.querySelector("#strands2");
+    
+    critB.addEventListener("mouseover", displayCritB);
 
         function displayCritB() {
             
             const critBStrands = criteriaArr.filter((data) => 
-            data.criterion == "B: Investigating")
-            const intro2 = document.querySelector("#intro2")
-            intro2.textContent ="At the end of year 5, students should be able to:"
-                const strandB1 = (critBStrands[0].strand)
-                const strandB2 = (critBStrands[1].strand)
-                const strandB3 = (critBStrands[2].strand)
-                const strandB4 = (critBStrands[3].strand)
-                const critBOne = document.querySelector("#p3")
-                critBOne.textContent = strandB1
-                const critBTwo = document.querySelector("#p4")
-                critBTwo.textContent = strandB2
-                const critBThree = document.querySelector("#p5")
-                critBThree.textContent = strandB3
-                const critBFour = document.querySelector("#p6")
-                critBFour.textContent = strandB4
+            data.criterion == "B: Investigating");
+
+            const intro2 = document.querySelector("#intro2");
+            intro2.textContent ="At the end of year 5, students should be able to:";
+                const strandB1 = (critBStrands[0].strand);
+                const strandB2 = (critBStrands[1].strand);
+                const strandB3 = (critBStrands[2].strand);
+                const strandB4 = (critBStrands[3].strand);
                 
-        critB.addEventListener("mouseout", hideCritB)
+                const critBOne = document.querySelector("#p3");
+                critBOne.textContent = strandB1;
+                
+                const critBTwo = document.querySelector("#p4");
+                critBTwo.textContent = strandB2;
+                
+                const critBThree = document.querySelector("#p5");
+                critBThree.textContent = strandB3;
+                
+                const critBFour = document.querySelector("#p6");
+                critBFour.textContent = strandB4;
+                
+        critB.addEventListener("mouseout", hideCritB);
                 function hideCritB() {
                     intro2.textContent = ' '
                     critBOne.textContent = ' '
                     critBTwo.textContent = ' '
                     critBThree.textContent = ' '
                     critBFour.textContent = ' '
-            }
-    }
-    const critC = document.querySelector("#strands3")
+            };
+    };
     
-    critC.addEventListener("mouseover", displayCritC)
+    const critC = document.querySelector("#strands3");
+    
+    critC.addEventListener("mouseover", displayCritC);
     
         function displayCritC() {
             
             const critCStrands = criteriaArr.filter((data) => 
-            data.criterion == "C: Communicating",)
-            const intro3 = document.querySelector("#intro3")
-            intro3.textContent ="At the end of year 5, students should be able to:"
-                const strandC1 = (critCStrands[0].strand)
-                const strandC2 = (critCStrands[1].strand)
-                const strandC3 = (critCStrands[2].strand)
-                const critCOne = document.querySelector("#p7")
-                critCOne.textContent = strandC1
-                const critCTwo = document.querySelector("#p8")
-                critCTwo.textContent = strandC2
-                const critCThree = document.querySelector("#p9")
-                critCThree.textContent = strandC3
+            data.criterion == "C: Communicating",);
+
+            const intro3 = document.querySelector("#intro3");
+            intro3.textContent ="At the end of year 5, students should be able to:";
+                const strandC1 = (critCStrands[0].strand);
+                const strandC2 = (critCStrands[1].strand);
+                const strandC3 = (critCStrands[2].strand);
+                
+                const critCOne = document.querySelector("#p7");
+                critCOne.textContent = strandC1;
+
+                const critCTwo = document.querySelector("#p8");
+                critCTwo.textContent = strandC2;
+
+                const critCThree = document.querySelector("#p9");
+                critCThree.textContent = strandC3;
         
-            critC.addEventListener("mouseout", hideCritC)
+            critC.addEventListener("mouseout", hideCritC);
                 function hideCritC() {
                     intro3.textContent = ' '
                     critCOne.textContent = ' '
                     critCTwo.textContent = ' '
                     critCThree.textContent = ' '
-            }
+            };
         
-    }
-    const critD = document.querySelector("#strands4")
+    };
+
+    const critD = document.querySelector("#strands4");
     
-    critD.addEventListener("mouseover", displayCritD)
+    critD.addEventListener("mouseover", displayCritD);
     
         function displayCritD() {
             
             const critDStrands = criteriaArr.filter((data) => 
-            data.criterion == "D: Thinking Critically",)
-            const intro4 = document.querySelector("#intro4")
-            intro4.textContent ="At the end of year 5, students should be able to:"
-                const strandD1 = (critDStrands[0].strand)
-                const strandD2 = (critDStrands[1].strand)
-                const strandD3 = (critDStrands[2].strand)
-                const strandD4 = (critDStrands[3].strand)
-                const critDOne = document.querySelector("#p10")
-                critDOne.textContent = strandD1
-                const critDTwo = document.querySelector("#p11")
-                critDTwo.textContent = strandD2
-                const critDThree = document.querySelector("#p12")
-                critDThree.textContent = strandD3
-                const critDFour = document.querySelector("#p13")
-                critDFour.textContent = strandD4
+            data.criterion == "D: Thinking Critically",);
+
+            const intro4 = document.querySelector("#intro4");
+            intro4.textContent ="At the end of year 5, students should be able to:";
+                const strandD1 = (critDStrands[0].strand);
+                const strandD2 = (critDStrands[1].strand);
+                const strandD3 = (critDStrands[2].strand);
+                const strandD4 = (critDStrands[3].strand);
+                
+                const critDOne = document.querySelector("#p10");
+                critDOne.textContent = strandD1;
+
+                const critDTwo = document.querySelector("#p11");
+                critDTwo.textContent = strandD2;
+
+                const critDThree = document.querySelector("#p12");
+                critDThree.textContent = strandD3;
+
+                const critDFour = document.querySelector("#p13");
+                critDFour.textContent = strandD4;
         
-                critD.addEventListener("mouseout", hideCritD)
+                critD.addEventListener("mouseout", hideCritD);
                 function hideCritD() {
                     intro4.textContent = ' '
                     critDOne.textContent = ' '
                     critDTwo.textContent = ' '
                     critDThree.textContent = ' '
                     critDFour.textContent = ' '
-            }
-
-    }
-}
+            };
+    };
+};
 
     const reflectionForm = document.querySelector("#reflection-form");
     const strategyName = document.querySelector("#strategy-name");
     const dropdown = document.querySelector("#strands-dropdown");
-    const reflectionInput = document.querySelector("#reflection");
+    const reflection1 = document.querySelector("#reflection1");
+    const reflection2 = document.querySelector("#reflection2");
+    const reflection3 = document.querySelector("#reflection3");
 
-    reflectionForm.addEventListener('submit', (e) => handleSubmit(e))
+    reflectionForm.addEventListener('submit', (e) => handleSubmit(e));
     
     function handleSubmit(e) {
         e.preventDefault(e);
-        const addedStrategy = document.querySelector("#added-strategy")
-        addedStrategy.textContent = strategyName.value
-        const addedStrand = document.querySelector("#assessd-criteria")
-        addedStrand.textContent = dropdown.value
-        const addedReflection = document.querySelector("#added-reflection")
-        addedReflection.textContent = reflectionInput.value
-        //const strategyCard = document.querySelector("#strategy-card").textContent = strategyName.value;
-        //here- add CSS to style card
-        //add different imputs to different parts of card in this function
+        const addedStrategy = document.querySelector("#added-strategy");
+        addedStrategy.textContent = strategyName.value;
 
+        const addedStrand = document.querySelector("#assessd-criterion");
+        addedStrand.textContent = dropdown.value;
 
-        // console.log(strategyName.value)
-         //console.log(reflectionInput.value)
-    }
+        const addedReflection1 = document.querySelector("#added-reflection1");
+        addedReflection1.textContent = reflection1.value;
+
+        const addedReflection2 = document.querySelector("#added-reflection2");
+        addedReflection2.textContent = reflection2.value;
+
+        const addedReflection3 = document.querySelector("#added-reflection3");
+        addedReflection3.textContent = reflection3.value;
+    };
     
